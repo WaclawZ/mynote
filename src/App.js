3f6a8f5e5d461e297e3d1de1e64b32553d08d76c
WaclawZ <waclaw.zalicki@gmail.com>
2019-06-09 14:08:10 +0200

    wrong commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
