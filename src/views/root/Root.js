bd02c6ce11dfcfb4f63a45bb4db74b2c991bbb80
WaclawZ <waclaw.zalicki@gmail.com>
2019-06-20 18:06:56 +0200

    Add button component

--- src/views/root/Root.js
+++ src/views/root/Root.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Button from 'components/Button/Button';
 
-const Root = () => <h1>Hello world</h1>;
+const Root = () => (
+  <div>
+    <h1>Hello world</h1>
+    <Button>Click me</Button>
+  </div>
+);
 
 export default Root;
