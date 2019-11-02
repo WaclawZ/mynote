c35c384c950adf30804e08daba35583ca22d36ce
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-02 22:12:28 +0100

    implement change color funcitonality to button in storybook

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import Button from './Button';
 
 export default {
   component: Button,
   title: 'Button',
+  decorators: [withKnobs],
 };
 
-export const primary = () => <Button>Primary button</Button>;
+export const primary = () => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(49, 100%, 58%)',
+    Secondary: 'hsl(196, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const groupId = 'GROUP-ID1';
+
+  const value = select(label, options, defaultValue, groupId);
+
+  return <Button color={value}>Primary button</Button>;
+};
 
 export const secondary = () => <Button secondary>Secondary button</Button>;
