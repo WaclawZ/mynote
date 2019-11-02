36fa522421b6ad28ff1e07d28c350242b2944980
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-02 22:11:30 +0100

    delete width attribute from button

--- src/views/root/Root.js
+++ src/views/root/Root.js
@@ -6,7 +6,7 @@ const Root = () => (
   <div>
     <GlobalStyle />
     <h1>Hello world</h1>
-    <Button width="150px">Close / Save</Button>
+    <Button>Close / Save</Button>
     <br />
     <Button secondary>Remove</Button>
   </div>
