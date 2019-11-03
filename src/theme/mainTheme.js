8501ddee886888c22db719b5248c668060654645
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-03 16:26:19 +0100

    add another shade of grey

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -4,6 +4,7 @@ export const theme = {
   tertiary: 'hsl(106, 47%, 64%)',
   grey100: 'hsl(0, 0%, 96%)',
   grey200: 'hsl(0, 0%, 90%)',
+  grey300: 'hsl(0, 0%, 70%)',
   black: 'hsl(0, 0%, 0%)',
   light: 300,
   bold: 600,
