d7332875de6acb14217159c9a0c75f61ddc030e0
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-03 17:44:57 +0100

    Add different ButtonIcons to storybook

--- src/components/atoms/ButtonIcon/ButtonIcon.stories.js
+++ src/components/atoms/ButtonIcon/ButtonIcon.stories.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 import plusIcon from 'assets/icons/plus.svg';
+import bulbIcon from 'assets/icons/bulb.svg';
+import logoutIcon from 'assets/icons/logout.svg';
+import penIcon from 'assets/icons/pen.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
 import ButtonIcon from './ButtonIcon';
+
+const YellowBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50rem;
+  height: 50rem;
+  background: ${({ theme }) => theme.primary};
+`;
 
 export default {
   component: ButtonIcon,
   title: 'ButtonIcon',
 };
 
-export const normal = () => <ButtonIcon icon={plusIcon}></ButtonIcon>;
+export const bulb = () => (
+  <YellowBackground>
+    <ButtonIcon icon={bulbIcon} />
+  </YellowBackground>
+);
+
+export const bulbActive = () => (
+  <YellowBackground>
+    <ButtonIcon active icon={bulbIcon} />
+  </YellowBackground>
+);
+
+export const logout = () => (
+  <YellowBackground>
+    <ButtonIcon icon={logoutIcon} />
+  </YellowBackground>
+);
+
+export const pen = () => (
+  <YellowBackground>
+    <ButtonIcon icon={penIcon} />
+  </YellowBackground>
+);
+
+export const plus = () => (
+  <YellowBackground>
+    <ButtonIcon icon={plusIcon} />
+  </YellowBackground>
+);
+
+export const twitter = () => (
+  <YellowBackground>
+    <ButtonIcon icon={twitterIcon} />
+  </YellowBackground>
+);
