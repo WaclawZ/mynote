be92384b9413c0b4871b623d2ebbae96622044e0
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-13 23:52:23 +0100

    style sidebar, implement page template, change logo

--- src/components/organisms/Sidebar/Sidebar.js
+++ src/components/organisms/Sidebar/Sidebar.js
@@ -1,21 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import logoIcon from 'assets/logo.svg';
 import penIcon from 'assets/icons/pen.svg';
 import twitterIcon from 'assets/icons/twitter.svg';
 import bulbIcon from 'assets/icons/bulb.svg';
 import logoutIcon from 'assets/icons/logout.svg';
 
-const Sidebar = () => (
-  <div>
-    <p>logo</p>
-    <div>
-      <ButtonIcon as={Link} to="/" icon={penIcon} />
-      <ButtonIcon as={Link} to="/twitters" icon={twitterIcon} />
-      <ButtonIcon as={Link} to="/articles" icon={bulbIcon} />
-    </div>
-    <ButtonIcon as={Link} to="/" icon={logoutIcon} />
-  </div>
+const StyledWrapper = styled.nav`
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 2.5rem 0;
+  width: 15rem;
+  height: 100vh;
+  background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : theme.note)};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledLogoLink = styled(NavLink)`
+  display: block;
+  width: 6.7rem;
+  height: 6.7rem;
+  background-image: url(${logoIcon});
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 80%;
+  border: none;
+  margin-bottom: 10vh;
+`;
+
+const StyledLinksList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const StyledLogoutButton = styled(ButtonIcon)`
+  margin-top: auto;
+`;
+
+const Sidebar = ({ pageType }) => (
+  <StyledWrapper activeColor={pageType}>
+    <StyledLogoLink to="/" />
+    <StyledLinksList>
+      <li>
+        <ButtonIcon exact as={NavLink} to="/" icon={penIcon} activeclass="active" />
+      </li>
+      <li>
+        <ButtonIcon as={NavLink} to="/twitters" icon={twitterIcon} activeclass="active" />
+      </li>
+      <li>
+        <ButtonIcon as={NavLink} to="/articles" icon={bulbIcon} activeclass="active" />
+      </li>
+    </StyledLinksList>
+    <StyledLogoutButton as={NavLink} to="/login" icon={logoutIcon} />
+  </StyledWrapper>
 );
+
+Sidebar.propTypes = {
+  pageType: PropTypes.string.isRequired,
+};
 
 export default Sidebar;
--- src/views/Root.js
+++ src/views/Root.js
@@ -6,15 +6,15 @@ import Twitters from 'views/Twitters';
 import Articles from 'views/Articles';
 
 const Root = () => (
-  <MainTemplate>
-    <BrowserRouter>
+  <BrowserRouter>
+    <MainTemplate>
       <Switch>
         <Route exact path="/" component={Notes} />
         <Route path="/twitters" component={Twitters} />
         <Route path="/articles" component={Articles} />
       </Switch>
-    </BrowserRouter>
-  </MainTemplate>
+    </MainTemplate>
+  </BrowserRouter>
 );
 
 export default Root;
