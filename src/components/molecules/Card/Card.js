3e440885a1bee2161460fbf27196cb4d0c976e5c
WaclawZ <waclaw.zalicki@gmail.com>
2019-11-13 15:55:57 +0100

    Add styled link button to card if article and avatar icon if twitter

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import Heading from 'components/atoms/Heading/Heading';
 import Button from 'components/atoms/Button/Button';
+import LinkIcon from 'assets/icons/link.svg';
 
 const StyledWrapper = styled.div`
   min-height: 38rem;
@@ -16,8 +17,13 @@ const StyledWrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
+  position: relative;
   padding: 1.7rem 3rem 1rem;
   background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
+
+  :first-of-type {
+    z-index: 99999;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -38,11 +44,37 @@ const StyledHeading = styled(Heading)`
   margin: 0.5rem 0 0;
 `;
 
+const StyledAvatar = styled.img`
+  width: 8.6rem;
+  height: 8.6rem;
+  border: 0.5rem solid ${({ theme }) => theme.twitter};
+  border-radius: 5rem;
+  position: absolute;
+  right: 2.5rem;
+  top: 2.5rem;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  width: 4.7rem;
+  height: 4.7rem;
+  border-radius: 5rem;
+  background: white url(${LinkIcon}) no-repeat;
+  background-size: 60%;
+  background-position: 50%;
+  position: absolute;
+  right: 2.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
 const Card = ({ cardType }) => (
   <StyledWrapper>
     <InnerWrapper activeColor={cardType}>
       <StyledHeading>Hello world</StyledHeading>
       <DateInfo>3 days</DateInfo>
+      {cardType === 'twitter' && <StyledAvatar src="https://avatars.io/twitter/hello_roman" />}
+      {cardType === 'article' && <StyledLinkButton href="https://google.com" />}
     </InnerWrapper>
     <InnerWrapper flex>
       <Paragraph>
